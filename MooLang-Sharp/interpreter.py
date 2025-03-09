58cbbc97e9338649f6e25addafdad61798df63fa
Python-Best-Coder <mqrshymelio@gmail.com>
2025-03-09 14:30:56 -0400

    Update interpreter.py
    Added many more operations (>, <, >=, <=) 
    Gave define stricter names 
    Added and, not operators 
    Added while loops, debugged for loops

--- MooLang-Sharp/interpreter.py
+++ MooLang-Sharp/interpreter.py
@@ -20,6 +20,21 @@ class Forl:
                 interpret(line)
         variables.pop(self.var)
         
+class Whilel:
+    def __init__(self,condition):  
+        self.c = condition 
+        self.lines = []
+
+    def add_line(self,line):
+        self.lines.append(line)
+
+    def run(self):
+        while True:
+            if not interpret(self.c):
+                break
+            for line in self.lines:
+                interpret(line)
+   
 
 class Statement:
     def __init__(self,condition):
@@ -38,16 +53,25 @@ class Function:
     def __init__(self,name,parameters):
         self.name = name
         self.param = parameters
+        self.extravars = []
         self.lines = []
     
     def add_line(self,line):
         self.lines.append(line)
+        makevar = re.search(r"\s*(\w+)\s*:\s*(\w+)\s*=\s*(\S+)\s*", line) 
+        if makevar:
+            if is_name(makevar.group(1).strip()):
+                self.extravars.append(makevar.group(1).strip())
 
     def run(self,param):
         for index,parameter in enumerate(self.param):
             variables[parameter] = param[index]
         for line in self.lines:
             interpret(line)
+        for p in self.param:
+            variables.pop(p)
+        for p in self.extravars:
+            variables.pop(p)
 
 
 variables = {}
@@ -65,20 +89,29 @@ def interpret(line):
     global variables,inside
     line = line.strip()
 
-    makevar = re.search(r"(.+): (.+) = (.+)",line)
+    makevar = re.search(r"\s*(\w+)\s*:\s*(\w+)\s*=\s*(\S+)\s*", line) 
     indexation = re.search(r"(.+)\[(.+)\]",line)
     out = re.search(r"console.out\((.+)\)",line)
-    sub = re.search(r"(.+)\-(.+)",line)
-    add = re.search(r"(.+)\+(.+)",line)
-    mul = re.search(r"(.+)\*(.+)",line)
-    div = re.search(r"(.+)\/(.+)",line)
-    eq = re.search(r"(.+) == (.+)",line)
+    add = re.search(r"\s*(\S+)\s*\+\s*(\S+)\s*", line) 
+    sub = re.search(r"\s*(\S+)\s*\-\s*(\S+)\s*", line)
+    mul = re.search(r"\s*(\S+)\s*\*\s*(\S+)\s*", line)
+    div = re.search(r"\s*(\S+)\s*/\s*(\S+)\s*", line)
+    eq  = re.search(r"\s*(\S+)\s*==\s*(\S+)\s*", line)
+    gr  = re.search(r"\s*(\S+)\s*\>\s*(\S+)\s*", line)
+    ls  = re.search(r"\s*(\S+)\s*\<\s*(\S+)\s*", line)
+    gre  = re.search(r"\s*(\S+)\s*\>=\s*(\S+)\s*", line)
+    lse  = re.search(r"\s*(\S+)\s*\<=\s*(\S+)\s*", line)
+    no_t = re.search(r"not (.+)",line)
+    ct = re.search(r"(.+)#\((.+)\)",line)
     autols = re.search(r"range\{(.+)\}",line)
-    ifst = re.search(r"if (.+) then \{",line)
-    forloop = re.search(r"for (.+) in (.+) \{",line)
+    ifst = re.search(r"if \((.+)\) then \{",line)
+    forloop = re.search(r"for (.+) in \((.+)\) \{",line)
+    whileloop = re.search(r"while \((.+)\) do \{",line)
     funct = re.search(r"define (.+) with (.+) \{",line)
+    an_d = re.search(r"(.+) and (.+)",line)
     if out:
         print(interpret(out.group(1)))
+        return interpret(out.group(1))
     elif makevar:
         if is_name(makevar.group(1).strip()):
             variables[makevar.group(1).strip()] = eval(f"{makevar.group(2)}({interpret(makevar.group(3))})")
@@ -89,8 +122,11 @@ def interpret(line):
         inside.append(Statement(interpret(ifst.group(1))))
     elif forloop:
         inside.append(Forl(forloop.group(1),interpret(forloop.group(2))))
+    elif whileloop:
+        inside.append(Whilel(whileloop.group(1)))
     elif funct:
-        inside.append(Function(funct.group(1),funct.group(2).split(",")))
+        if is_name(funct.group(1)):
+            inside.append(Function(funct.group(1),funct.group(2).split(",")))
     elif indexation:
         return variables[interpret(indexation.group(1))][int(interpret(indexation.group(2)))]
     elif autols:
@@ -100,16 +136,27 @@ def interpret(line):
         return x
         
     elif sub:
-        return int(interpret(sub.group(1))) - int(interpret(sub.group(2)))
+        return interpret(sub.group(1)) - interpret(sub.group(2))
     elif add:
-        return int(interpret(add.group(1))) + int(interpret(add.group(2)))
+        return interpret(add.group(1)) + interpret(add.group(2))
     elif div:
-        return int(interpret(div.group(1))) / int(interpret(div.group(2)))
+        return interpret(div.group(1)) / interpret(div.group(2))
     elif mul:
-        return int(interpret(mul.group(1))) * int(interpret(mul.group(2)))
+        return interpret(mul.group(1)) * interpret(mul.group(2))
     elif eq:
         return interpret(eq.group(1)) == interpret(eq.group(2))
-    
+    elif gr:
+        return interpret(gr.group(1)) > interpret(gr.group(2))
+    elif ls:
+        return interpret(ls.group(1)) < interpret(ls.group(2))
+    elif gre:
+        return interpret(gre.group(1)) >= interpret(gre.group(2))
+    elif lse:
+        return interpret(lse.group(1)) <= interpret(lse.group(2))
+    elif no_t:
+        return not interpret(no_t.group(1))
+    elif an_d:
+        return interpret(an_d.group(1)) and interpret(an_d.group(2))
     elif line in variables:
         return variables[line]
     elif line.startswith("$") and line[1:] in variables:
@@ -124,6 +171,10 @@ def interpret(line):
                 for itex in call.group(1).split(","):
                     p.append(interpret(itex))
                 name[1].run(p)
+                return name[1]
+    if ct:
+        return eval(f"{ct.group(1)}({ct.group(2)})")
+
     
     return line
 
@@ -133,7 +184,7 @@ def work(p):
     with open(p,"r") as file:
         for line in file.read().splitlines():
             line = line.strip()
-            if inside == [] or not isinstance(inside[-1],Statement) and not isinstance(inside[-1],Forl) and not isinstance(inside[-1],Function):
+            if inside == [] or not isinstance(inside[-1],Statement) and not isinstance(inside[-1],Forl) and not isinstance(inside[-1],Function) and not isinstance(inside[-1],Whilel):
                 x = interpret(line)
             else:
                 if len(inside) >= 1:
@@ -149,12 +200,19 @@ def work(p):
                         else:
                             inside[-1].run()
                             inside.pop()
+                    elif isinstance(inside[-1],Whilel):
+                        if line != "}":
+                            inside[-1].add_line(line)
+                        else:
+                            inside[-1].run()
+                            inside.pop()
                     elif isinstance(inside[-1],Function):
                         if line != "}":
                             inside[-1].add_line(line)
                         else:
                             functions.append((inside[-1].name,inside[-1]))
                             inside.pop()
+
 
 
             if x == "Terminate_*":
